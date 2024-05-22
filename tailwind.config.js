795e2cdb85dd0057a5620bce4153ca08b7919b00
aldomarsendo <aldo1marsendo@gmail.com>
2024-05-22 14:56:27 +0700

    Allbooks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary1: "#B46C10",
+        secondary: "#FDBA21",
+      },
+    },
   },
   plugins: [],
 };
